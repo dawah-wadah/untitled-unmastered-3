563d427ddc20706e2241488bda96d87d7e072016
Wadah Adlan <wadahadlan@gmail.com>
2017-09-08 21:12:51 -0400

    Dispatcher complete

--- util/dispatcher.js
+++ util/dispatcher.js
@@ -1,8 +1,32 @@
 import Drone from '../lib/drone.js';
 import Package from '../lib/package.js';
 
-const dispatch = (foo) => {
+const dispatcher = (foo) => {
+  const orderedDrones = Drone.soonestDrones(foo.drones);
+  const orderedPackages = Drone.soonestPackages(foo.packages);
+  const unassignedPackages = [];
+  const assignedPackages = [];
 
+  orderedDrones.forEach( drone => {
+    let parcel;
+    if (orderedPackages.length) {
+      parcel = orderedPackages.shift();
+      if (drone.undeliverable(parcel)) {
+        unassignedPackages.push({packageId: parcel.id});
+        parcel = orderedPackages.shift();
+        if (!parcel) return;
+      }
+      assignedPackages.push({droneId: drone.id, packageId: parcel.id});
+    } else {
+      return;
+    }
+  });
+
+  orderedPackages.map((parcel) => {
+    unassignedPackages.push({packageId: parcel.id});
+  });
+
+  return {assignedPackages, unassignedPackages};
 };
 
-export default dispatch;
+export default dispatcher;
